46ca3244e499da780829634279c3e48bbb733742
Didier Durand <didduran@amazon.ch>
2025-05-02 12:50:23 +0200

    Adding support for authorization at http layer

--- samples/python/common/client/client.py
+++ samples/python/common/client/client.py
@@ -23,13 +23,14 @@ import json
 
 
 class A2AClient:
-    def __init__(self, agent_card: AgentCard = None, url: str = None):
+    def __init__(self, agent_card: AgentCard = None, url: str = None, auth=None):
         if agent_card:
             self.url = agent_card.url
         elif url:
             self.url = url
         else:
             raise ValueError("Must provide either agent_card or url")
+        self.auth=auth
 
     async def send_task(self, payload: dict[str, Any]) -> SendTaskResponse:
         request = SendTaskRequest(params=payload)
@@ -55,9 +56,14 @@ class A2AClient:
         async with httpx.AsyncClient() as client:
             try:
                 # Image generation could take time, adding timeout
-                response = await client.post(
-                    self.url, json=request.model_dump(), timeout=30
-                )
+                if self.auth is None:
+                    response = await client.post(
+                        self.url, json=request.model_dump(), timeout=30
+                    )
+                else:
+                    response = await client.post(
+                        self.url, json=request.model_dump(), timeout=30, auth=self.auth
+                    )
                 response.raise_for_status()
                 return response.json()
             except httpx.HTTPStatusError as e:
