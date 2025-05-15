928c19ac14745a0fae6554753f784e5acea3532c
GhostC <1276537536@qq.com>
2025-05-15 22:08:13 +0800

    fix: A2AClientHTTPError(HTTP Error 400: timed out)

--- samples/python/common/client/client.py
+++ samples/python/common/client/client.py
@@ -51,7 +51,7 @@ class A2AClient:
         self, payload: dict[str, Any]
     ) -> AsyncIterable[SendTaskStreamingResponse]:
         request = SendTaskStreamingRequest(params=payload)
-        with httpx.Client(timeout=None) as client:
+        with httpx.Client(timeout=self.timeout) as client:
             with connect_sse(
                 client, 'POST', self.url, json=request.model_dump()
             ) as event_source:
