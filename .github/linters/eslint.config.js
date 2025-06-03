6681de60a5bf92516df2bee6bf45a7736198b9c8
Holt Skinner <holtskinner@google.com>
2025-06-03 14:34:15 -0500

    Add eslint config

---
+++ .github/linters/eslint.config.js
@@ -0,0 +1,13 @@
+module.exports = [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+    {
+      rules: {
+        'n/no-unsupported-features/es-syntax': 'off',
+      },
+    },
+  ];
