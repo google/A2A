0539d2da8086cacdfb8e4155cc93d634c10e55c9
Eden Reich <eden.reich@gmail.com>
2025-07-08 22:10:18 +0200

    style: Fix linting
    Signed-off-by: Eden Reich <eden.reich@gmail.com>

--- types/src/types.ts
+++ types/src/types.ts
@@ -277,7 +277,7 @@ export interface GetTaskPushNotificationConfigParams extends TaskIdParams {
 
 // --8<-- [start:ListTaskPushNotificationConfigParams]
 /** Parameters for getting list of pushNotificationConfigurations associated with a Task */
-export interface ListTaskPushNotificationConfigParams extends TaskIdParams { }
+export interface ListTaskPushNotificationConfigParams extends TaskIdParams {}
 // --8<-- [end:ListTaskPushNotificationConfigParams]
 
 // --8<-- [start:DeleteTaskPushNotificationConfigParams]
